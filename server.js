55754bc9a058fa4b283901b9f6169d5cce58fe3c
Pedro Felipe <pfsilva190406@gmail.com>
2025-02-24 19:12:54 -0300

    server.js com logs

--- server.js
+++ server.js
@@ -3,3 +3,13 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+  console.log("Request recebida pelo app node");
+});
+
+app.listen(port, () => {
+    console.log('App rodando na porta ${port}');
+});
